316db0097c06097484727ea5d57f0bf0c39b1d92
Kato Junichi <katoj62@gmail.com>
2024-07-11 13:54:08 +0900

    hatu2

--- src/App.js
+++ src/App.js
@@ -10,16 +10,12 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <h2>
-          <FontAwesomeIcon icon={faDumbbell} /> 筋トレ記録・分析アプリ
-        </h2>
+        
         <Routes>
           <Route path="/" element={<WorkoutForm />} />
           <Route path="/graph" element={<Graph />} />
         </Routes>
-        <Link to="/graph">
-          <FontAwesomeIcon icon={faChartLine} /> 進捗グラフを見る
-        </Link>
+        
       </div>
     </Router>
   );
